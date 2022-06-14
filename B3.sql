9aa905f384488f2f7f0410109878924f4fc397a3
apirantsk20 <57123254+apirantsk20@users.noreply.github.com>
2022-06-13 23:23:39 -0700

    Delete B3.sql

--- B3.sql
+++
@@ -1,8 +0,0 @@
---B3 
-SELECT  customerID	    AS CustomerID,
-		companyName     AS CompanyName,
-		country		    AS Country,
-		phone			AS Phone
-FROM   customers
-WHERE  country IN ('Canada','USA')
-ORDER BY customerID   
