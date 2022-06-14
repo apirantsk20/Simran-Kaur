405019693bef4326a3dda064b9b429b6b1e81e0c
apirantsk20 <57123254+apirantsk20@users.noreply.github.com>
2022-06-13 23:23:18 -0700

    Delete B10.sql

--- B10.sql
+++
@@ -1,5 +0,0 @@
---B10
-SELECT  SUBSTRING (productName,1,1)    AS ProductName,
-COUNT (productName)      AS Total
-FROM   products
-GROUP BY SUBSTRING (productName,1,1) HAVING  COUNT (productName) >= 3 
